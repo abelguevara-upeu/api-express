e96005c77a28e2809d41489bb2ba24f817ae68b9
abelguevarah <abelguevara@upeu.edu.pe>
2024-12-25 10:22:42 -0500

    Update: CORS fixed again :')

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(cors(
                 `http://localhost:${PORT_HTTP}`,
             ];
             if (ACCEPTED_ORIGINS.includes(origin)) {
-                callback(null, true);
+                return callback(null, true);
             }
 
             if (!origin) {
-                callback(null, true);
+                return callback(null, true);
             }
 
             return callback(new Error('Not allowed by CORS'));
